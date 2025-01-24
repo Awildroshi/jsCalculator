96a9f6005e98c3ff63a6f5575b7cb03afad86089
Awildroshi <151027830+Awildroshi@users.noreply.github.com>
2025-01-24 22:32:43 +0530

    Update main.js

--- main.js
+++ main.js
@@ -24,6 +24,7 @@ function calculate(){
     catch(error){
         display.value="Error";
     }
+     
 }
 //keybaord support for enter,clear and backspace
 document.addEventListener('keydown', (e) => {
